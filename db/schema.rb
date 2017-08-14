da586004b4aaf53fa4e3e88be8e480e422ca1e9f
Hamza Nebolsi <hamza@ebby.no>
2017-08-14 17:23:22 +0200

    psql something fix

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170813165449) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "currencies", force: :cascade do |t|
     t.string   "name"
     t.string   "symbol"
